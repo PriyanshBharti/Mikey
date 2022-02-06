95353b5994693029844d6517cefea4dfe1bb9394
Priyansh Bharti <89295547+PriyanshBharti@users.noreply.github.com>
2022-02-06 14:31:46 +0530

    Update alive.py

--- Tanji/modules/alive.py
+++ Tanji/modules/alive.py
@@ -19,6 +19,6 @@ async def awake(event):
   TEXT += f"✯ **Library Version :** `{telever}` \n\n"
   TEXT += f"✯ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"✯ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**✴ Thanks For Adding Me Here ✴**"
+  TEXT += "**⋨ Thanks For Adding Me Here ⋩**"
   BUTTON = [[Button.url("✢ Help ✢", "https://t.me/Kaneki_Ken_Robot?start=help"), Button.url("✢ Support ✢", "https://t.me/=KanekiSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
