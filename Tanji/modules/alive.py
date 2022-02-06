f6b1ca33750bff1763a64126c914af3328d9353c
Priyansh Bharti <89295547+PriyanshBharti@users.noreply.github.com>
2022-02-06 14:08:00 +0530

    Update alive.py

--- Tanji/modules/alive.py
+++ Tanji/modules/alive.py
@@ -13,12 +13,12 @@ PHOTO = "https://telegra.ph/file/dcdbb6365975d066015c7.mp4"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Mikey.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Ken Kaneki.** \n\n"
   TEXT += f"✯ **I'm Working Properly** \n\n"
-  TEXT += f"✯ **Honorable Commander : [Commander](https://t.me/Darling_Hiro)** \n\n"
+  TEXT += f"✯ **My Owner : [Owner](https://t.me/Darling_Hiro)** \n\n"
   TEXT += f"✯ **Library Version :** `{telever}` \n\n"
   TEXT += f"✯ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"✯ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
+  TEXT += "**✴ Thanks For Adding Me Here ✴**"
   BUTTON = [[Button.url("✢ Help ✢", "https://t.me/Kaneki_Ken_Robot?start=help"), Button.url("✢ Support ✢", "https://t.me/=KanekiSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
