364d4c8561b5a4f3739906c6c67a6f0617625c49
Priyansh Bharti <89295547+PriyanshBharti@users.noreply.github.com>
2022-02-06 12:27:51 +0530

    Update __main__.py

--- Tanji/__main__.py
+++ Tanji/__main__.py
@@ -360,15 +360,15 @@ def tanji_about_callback(update, context):
     query = update.callback_query
     if query.data == "tanji_":
         query.message.edit_text(
-            text="๏ I'm *Tanjirou*, a powerful group management bot built to help you manage your group easily."
+            text="๏ I'm *Ken Kaneki*, a powerful group management bot built to help you manage your group easily."
             "\n• I can restrict users."
             "\n• I can greet users with customizable welcome messages and even set a group's rules."
             "\n• I have an advanced anti-flood system."
             "\n• I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc."
             "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords."
             "\n• I check for admins' permissions before executing any command and more stuffs"
-            "\n\n_Tanjirou's licensed under the GNU General Public License v3.0_"
-            "\n\n Click on button bellow to get basic help for EmikoRobot.",
+            "\n\n_Kaneki's licensed under the GNU General Public License v3.0_"
+            "\n\n Click on button bellow to get basic help for Kaneki_Ken_Robot.",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -382,7 +382,7 @@ def tanji_about_callback(update, context):
                     InlineKeyboardButton(text="Credits", callback_data="tanji_credit"),
                  ],
                  [
-                    InlineKeyboardButton(text="Owner", url="https://t.me/Lynncept_iz_here"),
+                    InlineKeyboardButton(text="Owner", url="https://t.me/Darling_Hiro"),
                 ],
                  [
                     InlineKeyboardButton(text="Go Back", callback_data="tanji_back"),
@@ -408,7 +408,7 @@ def tanji_about_callback(update, context):
     elif query.data == "tanji_admin":
         query.message.edit_text(
             text=f"*๏ Let's make your group bit effective now*"
-            "\nCongragulations, Tanjirou Kamado now ready to manage your group."
+            "\nCongragulations, Ken Kaneki now ready to manage your group."
             "\n\n*Admin Tools*"
             "\nBasic Admin tools help you to protect and powerup your group."
             "\nYou can ban members, Kick members, Promote someone as admin through commands of bot."
@@ -435,14 +435,14 @@ def tanji_about_callback(update, context):
         )
     elif query.data == "tanji_support":
         query.message.edit_text(
-            text="*๏ Tanjirou support chats*"
+            text="*๏ Kaneki Support Chat*"
             "\nJoin My Support Group/Channel for see or report a problem on Emiko.",
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="Support", url="t.me/Tanji_kamado_bot"),
-                    InlineKeyboardButton(text="Updates", url="https://t.me/Tanjirou_updates"),
+                    InlineKeyboardButton(text="Support", url="t.me/KanekiSupport"),
+                    InlineKeyboardButton(text="Updates", url="https://t.me/KanekiUpdates"),
                  ],
                  [
                     InlineKeyboardButton(text="Go Back", callback_data="tanji_"),
