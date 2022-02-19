9eb25eb3fcb2db400d3a56b97d0f7405e9320fbb
Priyansh Bharti <89295547+PriyanshBharti@users.noreply.github.com>
2022-02-20 00:35:19 +0530

    Update userinfo.py

--- Tanji/modules/userinfo.py
+++ Tanji/modules/userinfo.py
@@ -468,8 +468,8 @@ def stats(update, context):
             status
             + "\n*Bot statistics*:\n"
             + "\n".join([mod.__stats__() for mod in STATS])
-            + f"\n\n[✦ Support](https://t.me/{SUPPORT_CHAT}) | [✦ Updates](https://t.me/Shinobu_Update_Channel)\n\n"
-            + "╘══「 by [Tamim Zaman](https://github.com/TamimZaman99) 」\n",
+            + f"\n\n[✦ Support](https://t.me/{SUPPORT_CHAT}) | [✦ Updates](https://t.me/KanekiUpdates)\n\n"
+            + "╘══「 by [Hiro](https://t.me/Darling_Hiro) 」\n",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
         )
@@ -481,9 +481,9 @@ def stats(update, context):
                         "\n*Bot statistics*:\n"
                         + "\n".join(mod.__stats__() for mod in STATS)
                     )
-                    + f"\n\n✦ [Support](https://t.me/{SUPPORT_CHAT}) | ✦ [Updates](https://t.me/Shinobu_Update_Channel)\n\n"
+                    + f"\n\n✦ [Support](https://t.me/{SUPPORT_CHAT}) | ✦ [Updates](https://t.me/KanekiUpdates)\n\n"
                 )
-                + "╘══「 by [Tamim Zaman](https://github.com/TamimZaman99) 」\n"
+                + "╘══「 by [Hiro](https://t.me/Darling_Hiro) 」\n"
             ),
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
