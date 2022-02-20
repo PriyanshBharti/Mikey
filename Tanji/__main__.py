4820d8bbe5c2d914ad6b6417a499734292ef5985
Priyansh Bharti <89295547+PriyanshBharti@users.noreply.github.com>
2022-02-21 00:00:23 +0530

    Update __main__.py

--- Tanji/__main__.py
+++ Tanji/__main__.py
@@ -6,9 +6,24 @@ import requests
 from sys import argv
 from typing import Optional
 
-from Tanji import (ALLOW_EXCL, CERT_PATH, DONATION_LINK, LOGGER,
-                          OWNER_ID, PORT, SUPPORT_CHAT, TOKEN, URL, WEBHOOK,
-                          dispatcher, StartTime, telethn, updater, pbot, DEV_USERS, DRAGONS, DEMONS, ALIVE_PIC)
+from Tanji import (
+    ALLOW_EXCL,
+    CERT_PATH,
+    DONATION_LINK,
+    LOGGER,
+    OWNER_ID,
+    PORT,
+    SUPPORT_CHAT,
+    TOKEN,
+    URL,
+    WEBHOOK,
+    SUPPORT_CHAT,
+    dispatcher,
+    StartTime,
+    telethn,
+    pbot,
+    updater,
+)
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
 from Tanji.modules import ALL_MODULES
