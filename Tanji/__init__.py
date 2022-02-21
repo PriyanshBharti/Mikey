16e26c7f4a70834a6f0ec0eaf4921fc2c5b6bdbe
Priyansh Bharti <89295547+PriyanshBharti@users.noreply.github.com>
2022-02-21 12:09:23 +0530

    Update __init__.py

--- Tanji/__init__.py
+++ Tanji/__init__.py
@@ -214,7 +214,7 @@ else:
         sw = None
         LOGGER.warning("Can't connect to SpamWatch!")
 
-from ShinobuRobot.modules.sql import SESSION
+from Tanji.modules.sql import SESSION
 
 defaults = tg.Defaults(run_async=True)
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
