a2dbb096eca110de86313519f1579b87613ffde8
Priyansh Bharti <89295547+PriyanshBharti@users.noreply.github.com>
2022-02-21 11:10:42 +0530

    Update __main__.py

--- Tanji/__main__.py
+++ Tanji/__main__.py
@@ -11,25 +11,9 @@ from sys import argv
 from typing import Optional
 from telegram import __version__ as peler
 from platform import python_version as memek
-from Tanji import (
-    ALLOW_EXCL,
-    CERT_PATH,
-    DONATION_LINK,
-    LOGGER,
-    OWNER_ID,
-    PORT,
-    SUPPORT_CHAT,
-    TOKEN,
-    URL,
-    WEBHOOK,
-    SUPPORT_CHAT,
-    BOT_NAME,
-    dispatcher,
-    StartTime,
-    telethn,
-    pbot,
-    updater,
-)
+from Tanji import (ALLOW_EXCL, CERT_PATH, DONATION_LINK, LOGGER,
+                          OWNER_ID, PORT, SUPPORT_CHAT, TOKEN, URL, WEBHOOK,
+                          dispatcher, StartTime, telethn, updater, pbot, DEV_USERS, DRAGONS, DEMONS, BOT_NAME, BOT_PIC, ALIVE_PIC)
 
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
