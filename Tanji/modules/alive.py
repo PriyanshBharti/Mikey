98109c9bce63255ddc453ab671498683bb814c8d
Priyansh Bharti <89295547+PriyanshBharti@users.noreply.github.com>
2022-03-05 13:14:10 +0530

    Update alive.py

--- Tanji/modules/alive.py
+++ Tanji/modules/alive.py
@@ -13,12 +13,12 @@ PHOTO = "https://telegra.ph/file/dcdbb6365975d066015c7.mp4"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Ken Kaneki.** \n\n"
-  TEXT += f"✯ **I'm Working Properly** \n\n"
-  TEXT += f"✯ **My Owner : [⁰¹⁶ ᕼɪʀᴏ](https://t.me/Darling_Hiro)** \n\n"
-  TEXT += f"✯ **Library Version :** `{telever}` \n\n"
-  TEXT += f"✯ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"✯ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**⋨ Thanks For Adding Me Here**"
+    text = f"*👋 Hey There* {USER} \n\n"
+    text += f"✨ *I'm {BOT_NAME}*\n🍀 *I'm Working Fine as always* \n\n"
+    text += f"*👑 Sun Breather:* [Devansh](tg://user?id=5288049130)\n"
+    text += f"*💻 My Devs :* [Devs of {BOT_NAME}](https://t.me/SunBreatherUpdates/10)\n\n"
+    text += "*⛅️ Bot version:* [Yorrichi 1.0](https://t.me/SunBreatherUpdates/)\n"
+    text += "*🐍 Python-Telegram-Bot:*" + str(ptbver) + "\n"
+    text += f"*⚡ Uptime:* {uptime}"
   BUTTON = [[Button.url("✢ Help ✢", "https://t.me/Kaneki_Ken_Robot?start=help"), Button.url("✢ Support ✢", "https://t.me/=KanekiSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
